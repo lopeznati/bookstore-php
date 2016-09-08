6a80aa2d1da19d4c7ad7ba8248626bd9dfe7b326
Martina Pintener <Martina Pintener>
2016-09-07 21:24:41 -0300

    Funciones 	-Agrega estilo a index.php 	-Cambio de menu-lateral.php, ahora hay una sola barra superior

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-	include_once "header.php";
+include_once "menu-lateral.php";
 ?>
+<head>
 	<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
 	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
 	<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
@@ -8,24 +9,17 @@
 
 		<link href="assets/css/bootstrap.css" rel="stylesheet">
     <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
-    <link rel="stylesheet" type="text/css" href="assets/css/zabuto_calendar.css">
-    <link rel="stylesheet" type="text/css" href="assets/js/gritter/css/jquery.gritter.css" />
-    <link rel="stylesheet" type="text/css" href="assets/lineicons/style.css">
-    <link href="assets/css/style.css" rel="stylesheet">
     <link href="assets/css/style-responsive.css" rel="stylesheet">
-    <script src="assets/js/chart-master/Chart.js"></script>
-
+		<script src="https://use.fontawesome.com/7fa1cd24b5.js"></script>
+</head>
+<body>
     <!-- MENU -->
-    <div class="container-fluid">
+	<div class="container-fluid">
 		<div class="row">
-		     <?php
-				include_once "menu-lateral.php";
-				?>
-
 			<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
 				<!-- Sector de busqueda -->
 				<?php include_once 'busq.php';?>
-				<h1 class="page-header">Libros</h1>
+				<h2 class="page-header">Libros</h2>
 				<?php
 					if(isset($_REQUEST['busqueda']))
 					{
@@ -39,7 +33,7 @@
 					}
 				?>
 				<div class="table-responsive">
-					<table class="table table-striped" class="modulobuscador" >
+					<table class="table table-hover" class="modulobuscador" >
 						<tbody>
 						<?php
 							while($l=mysql_fetch_array($libros))
@@ -58,8 +52,8 @@
 										<span class='titulos'>Precio: </span>"."$ ".$l['precio']."</br>
 										<span class='titulos'>Categoria: </span>".$c['descripcion']."</br>
 										<span class='titulos'>Editorial: </span>".$e['nombre']."</br>
-										<div class='celdaboton'><a href='detalle.php?id=".$l['id']."'><botton class='btn btn-primary'>+Info</botton></a>
-											<input  type='submit' class='btn btn-primary' value='Comprar'/>
+										<div class='celdaboton'><a href='detalle.php?id=".$l['id']."'><botton class='btn btn-sunny text-uppercase'><i class='fa fa-plus' aria-hidden='true'></i>Info</botton></a>
+											<input  type='submit' class='btn btn-fresh text-uppercase' value='Comprar'/>
 											<!--<input  type='submit' class='btn btn-primary' value='+Info'/>
 											<botton id='cartelLibro' class='btn btn-primary'> Carro</botton>
 											<a href='detalle.php'><botton class='btn btn-primary'> Carro</botton></a>-->
--- verCarro.php
+++ verCarro.php
@@ -1,12 +1,14 @@
 ﻿<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
 <script type="text/javascript"  src="js/scripts.js"></script>
-<?php 
+<link href="https://fonts.googleapis.com/css?family=Crete+Round|Oswald|Roboto" rel="stylesheet">
+<link href="styles.css" rel="stylesheet">
+<?php
 include_once 'header.php';
 
 //si lo agrego una segunda vez
 if(isset($_SESSION['carro'])){
 	if(isset($_REQUEST['id'])){
-		
+
 		$arreglo=$_SESSION['carro'];
 		$encontro=false;
 		$numero=0;
@@ -34,9 +36,9 @@ if(isset($_SESSION['carro'])){
 			array_push($arreglo,$datosNuevos);
 			$_SESSION['carro']=$arreglo;
 		}
-	
+
 	}
-	
+
 }else{
 	//primera vez
 	if(isset($_REQUEST['id'])){
@@ -55,53 +57,53 @@ if(isset($_SESSION['carro'])){
 	}
 }
 
-		
+
 
 ?>
 
 
  <div class="container-fluid">
       <div class="row">
-	   <?php 
-		
+	   <?php
+
 		include_once "menu-lateral.php";
-		
+
 		?>
 		<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-		
+
 		<h1 align="center">Carrito</h1>
-		
-		<?php 
+
+		<?php
 		if(isset($_SESSION['carro'])){
 			$datos=$_SESSION['carro'];
 			$total=0;
-			
-			
-			
-			
-		
-		
-		
-		//si el carro no está vacío,
-		//mostramos los productos 
-		?>
-		
-		<table width="800" border="0" cellspacing="0" cellpadding="0" align="center">
-		<tr bgcolor="#333333" class="tit"> 
-		<td width="105">Libro</td>
-		<td width="207">Precio</td>
-		<td width='305' >Cantidad de Unidades</td>
-		<td width='105'>SubTotal</td>
 
-		<td width="10" align="center">Borrar</td>
-		
-		</tr>
+
+
+
+
+
+
+		//si el carro no está vacío,
+		//mostramos los productos
+		?>
+
+		<table width="800" border="0" cellspacing="0" cellpadding="0" align="center">
+			<thead>
+				<tr>
+					<th>Libro</th>
+					<th>Precio</th>
+					<th>Cantidad de unidades</th>
+					<th>Subtotal</th>
+					<th>Borrar</th>
+				</tr>
+			</thead>
 		<?php
-		$color=array("#ffffff","#F0F0F0");
+		$color=array("#ffffff","#D9D4C8");
 		$contador=0;
 		//las 2 líneas anteriores
 		//sirven sólo para hacer
-		//una tabla con colores 
+		//una tabla con colores
 		//alternos
 		$suma=0;
 		//antes de recorrer todos
@@ -111,21 +113,21 @@ if(isset($_SESSION['carro'])){
 		//iremos sumando los subtotales
 		//del costo de cada item por la
 		//cantidad de unidades que se
-		//especifiquen 
+		//especifiquen
 		foreach($datos as $k => $v){
 		//recorremos la matriz que tiene
-		//todos los valores del carro, 
+		//todos los valores del carro,
 		//calculamos el subtotal y el
-		// total 
+		// total
 		$subto=$v['cantidad']*$v['precio'];
-		
+
 		$suma=$suma+$subto;
 		$contador++;
 		//este es el contador que usamos
-		//para los colores alternos 
+		//para los colores alternos
 		?>
-		<div class="producto_<?php echo $v['id']; ?>"> 
-		<tr bgcolor="<?php echo $color[$contador%2]; ?>" class='prod'> 
+		<div class="producto_<?php echo $v['id']; ?>">
+		<tr bgcolor="<?php echo $color[$contador%2]; ?>" class='prod'>
 		<td><?php echo $v['titulo'] ?></td>
 		<td><?php echo $v['precio'] ?></td>
 		<td><input  type='text' value='<?php echo $v['cantidad'];?>'
@@ -134,11 +136,11 @@ if(isset($_SESSION['carro'])){
 					class="cantidad"></td>
 		<td><span class='subtotal_<?php echo $v['id']; ?>'><?php echo $v['precio']*$v['cantidad'];?></span></td>
 		<td align="center"><a href="borracar.php?id=<?php echo $v['id'] ?>"><img src="trash.gif" width="12" height="14" border="0"></a></td>
-		
-		
-		
+
+
+
 		<input name="id" type="hidden" id="id" value="<?php echo $v['id'] ?>"> </td>
-	
+
 
 		</tr>
 		</div>
@@ -146,54 +148,54 @@ if(isset($_SESSION['carro'])){
 		//por cada item creamos un
 		//formulario que submite a
 		//agregar producto y un link
-		//que permite eliminarlos 
+		//que permite eliminarlos
 		}
 		?>
 		</table>
 		</div>
-		
+
 		<div style="margin-left:250px;" align="center"><span class="prod">Total de Artículos: <?php echo count($datos);
 		//el total de items va a ser igual
 		//a la cantidad de elementos que
 		//tenga la matriz $carro, valor
 		//que obtenemos con la función
-		//count o con sizeof 
-		?></span> 
+		//count o con sizeof
+		?></span>
 		</div><br>
 		<div style="margin-left:250px;" align="center" id='total'><span class="prod">Total: $<?php echo number_format($suma,2);
 		//mostramos el total de la variable
-		//$suma formateándola a 2 decimales 
-		?></span> 
+		//$suma formateándola a 2 decimales
+		?></span>
 		</div><br>
-		
+
 		<div  style="margin-left:250px;" align="center"><span class="prod">Continuar la selección de productos</span>
-		
-		
+
+
 		<a href="index.php?<?php echo SID;?>">
-		<img src="continuar.gif" width="13" height="13" border="0"></a> 
+		<img src="continuar.gif" width="13" height="13" border="0"></a>
 		</div>
 		<?php if($suma!=0){ ?>
-		<div  style="margin-left:900px;" align="center"><a href='comprar.php'><botton class='btn btn-primary'>Iniciar Compra</botton></a> </div>
+		<div  style="margin-left:900px;" align="center"><a href='comprar.php'><botton class='btn btn-fresh text-uppercase'>Iniciar Compra</botton></a> </div>
 		<?php }?>
 		<?php }else{ ?>
-		
+
 		<p align="center"> <span class="prod">No hay productos seleccionados</span>
 		<a href="index.php?<?php echo SID;?>">
-		<img src="continuar.gif" width="13" height="13" border="0"></a> 
+		<img src="continuar.gif" width="13" height="13" border="0"></a>
 		<?php }?>
 		</p>
-			   
 
-             
-               
-                
-		  
-          
+
+
+
+
+
+
           <div class="table-responsive">
             <table class="table table-striped">
-              
+
             </table>
-          
+
         </div>
       </div>
     </div>
@@ -208,8 +210,7 @@ if(isset($_SESSION['carro'])){
     <script src="assets/js/vendor/holder.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
-	
-	
+
+
   </body>
 </html>
-
