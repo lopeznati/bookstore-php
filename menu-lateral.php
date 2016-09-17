93f11c61d24f51f1d764dbe965b0a482f70d627f
Martina <mpintener@gmail.com>
2016-09-17 16:33:04 -0300

    Agrega colores a los links

--- menu-lateral.php
+++ menu-lateral.php
@@ -45,19 +45,17 @@ include_once "funciones.php";
           </button>
           <a class="navbar-brand" href="index.php">Todo Colección</a>
         </div>
-
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="navbar-collapse-2">
           <ul class="nav navbar-nav navbar-right">
             <?php
 			if(!isset($_SESSION['rol']) or !isset($_SESSION['usuario_valido']) or $_SESSION['usuario_valido']!=TRUE )
-            {
-            ?>
+            { ?>
             <li><a href="miCuenta.php">Mi Cuenta</a></li>
-			<li><a href="formularioContacto.php">Contacto</a></li>
-      <li><a href="verCarro.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a></li>
-      <li><a href="mapaSitio.php">Mapa del Sitio</a></li>
-			<li><a class="btn btn-default btn-outline btn-circle collapsed"  href="signin.php" aria-expanded="false" >Iniciar Sesión</a></li>
+			      <li><a href="formularioContacto.php">Contacto</a></li>
+            <li><a href="verCarro.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a></li>
+            <li><a href="mapaSitio.php">Mapa del Sitio</a></li>
+			      <li><a class="btn btn-default btn-outline btn-circle collapsed"  href="signin.php" aria-expanded="false" >Iniciar Sesión</a></li>
             <?php
             }
 			else if (isset($_SESSION['rol']))
